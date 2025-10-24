882b5a63642141b7f1da66fe699f46cf2cbabc5a
devStormAkash <iitjee523@gmail.com>
2025-10-24 09:15:18 +0530

    Add necesaary css

--- client/src/pages/VideoRoom.jsx
+++ client/src/pages/VideoRoom.jsx
@@ -172,7 +172,7 @@ const VideoRoom = () => {
 
   return (
     <>
-      <div className="w-full h-screen bg-purple-300  flex flex-col items-center justify-start">
+      <div className="w-full min-h-screen bg-purple-300  flex flex-col items-center justify-start">
         <h1 className="text-5xl font-semibold text-center">Video Room</h1>
         {remoteSocketId && (
           <p className="border p-3 rounded bg-green-100 text-xl my-4 text-green-800">
